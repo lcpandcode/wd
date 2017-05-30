74d75a262ce4371ae2e68d4cab03ad7543a83cfe
lcpandcode <1140823948@qq.com>
2017-05-30 20:55:01 +0800

    完善了获取粉丝列表 完善了首页，根据登录与非登录进行了不同首页的展示 修复了一些bug

---
+++ src/test/java/cn/cpliang/wenda/SystemTest.java
@@ -0,0 +1,10 @@
+package cn.cpliang.wenda;
+
+/**
+ * Created by lcp on 17-5-29.
+ */
+public class SystemTest {
+    public static void main(String [] args){
+        System.out.println("test");
+    }
+}
